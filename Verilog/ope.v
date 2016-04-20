eb80403e809c96bbba0d7af9e3bf6a83fc118e42
Norihiko Nakasato <n.norihiko1229@gmail.com>
2016-04-20 10:34:38 +0900

    Verilog 比較器

--- Verilog/ope.v
+++ Verilog/ope.v
@@ -1,3 +1,72 @@
+// 8bit比較器 a > b -> g:1 e:0, a == b -> 01, a < b -> 00
+module comp_8(
+    input wire [8-1:0] ina, inb,
+    output wire g, 
+    output wire e);
+    
+    assign g = (ina > inb);
+    assign e = (ina == inb);
+endmodule 
+
+// 32bit比較器
+// a > b -> 10, a < b -> 00, a == b -> 01を返す
+module comp_32(
+    input  wire [32-1:0] ina, inb,
+    input  wire clk,
+    input  wire rst_n, 
+    output reg  g,
+    output reg  e);
+
+    reg [3-1:0] count;
+    reg [32-1:0] rega, regb;
+    wire [8-1:0] a,b;
+    wire g8, e8;
+
+    comp_8 comp_8(a, b, g8, e8); 
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            count <= 0;
+            g <= 0;
+            e <= 0;
+            rega <= ina;
+            regb <= inb;
+            a <= ina[32-1:24];
+            b <= inb[32-1:24];
+        end else begin
+            if (g8 == 1) begin
+                g <= 1;
+                e <= 0;
+            end else if (!e8) begin
+                g <= 0;
+                e <= 0;
+            end else begin 
+                case (count)
+                    3'b000 : begin
+                        a <= rega[24-1:16];
+                        b <= regb[24-1:16];
+                        count <= count + 1;
+                    end
+                    3'b001 : begin
+                        a <= rega[16-1:8];
+                        b <= regb[16-1:8];
+                        count <= count + 1;
+                    end
+                    3'b010 : begin
+                        a <= rega[8-1:0];
+                        b <= regb[8-1:0];
+                        count <= count + 1;
+                    end
+                    default : begin
+                        g <= 0;
+                        e <= 1;
+                    end
+                endcase
+            end
+        end
+    end
+endmodule
+
 // 32bitと32bitの加算器
 module add_32(
     input  wire [32-1:0] ina, inb,
@@ -57,5 +126,64 @@ module add_32(
 	    endcase
         end
     end
-endmodule
+endmodule 
+
+module sub_32(
+    input  wire [32-1:0] ina, inb,
+    input  wire clk,
+    input  wire rst_n,
+    output reg [64-1:0] result);
     
+    reg  [3-1:0] count;
+    reg [32-1:0] rega, regb;
+    wire [9-1:0] res;
+    reg  [8-1:0] a,b;
+    reg carry;
+
+    assign res = a + b + carry;
+
+    always @(posedge clk) begin
+        //reset operations
+        if (!rst_n) begin
+            count <= 0;
+            result [64-1:0] <= 0;
+            carry <= 0;
+	        rega <= ina;
+	        regb <= inb;
+            a <= ina[8-1:0];
+            b <= inb[8-1:0];
+        end else begin
+            // RT operations
+            case (count)
+                3'b000 : begin
+                    a <= rega[16-1:8];
+                    b <= regb[16-1:8];
+		            carry <= res[9-1];
+		            result[8-1:0] <= res[8-1:0];
+		            count <= count + 1;
+                end
+                3'b001 : begin
+                    a <= rega[24-1:16];
+                    b <= regb[24-1:16];
+		            result[16-1:8] <= res[8-1:0];
+		            carry <= res[9-1];
+		            count <= count + 1;
+                end
+                3'b010 : begin
+                    a <= rega[32-1:24];
+                    b <= regb[32-1:24];
+		            result[24-1:16] <= res[8-1:0];
+		            carry <= res[9-1];
+		            count <= count + 1;
+                end
+		        3'b011 : begin
+		            result[32-1:24] <= res[8-1:0];
+		            count <= count + 1;
+		            carry <= res[9-1];
+		        end
+		        default : begin
+		        end
+	        endcase
+        end
+    end
+endmodule 
--- Verilog/opesim.v
+++ Verilog/opesim.v
@@ -1,32 +1,29 @@
-module add_32sim();
-    reg [32-1:0] ina, inb;
+module comp_8sim();
+    reg [8-1:0] ina, inb;
     reg clk, rst_n;
-    wire [64-1:0] result;
-    wire [64-1:0] inab;
+    wire g, e;
 
-    assign inab = ina + inb;
-
-    add_32 add_32(ina, inb, clk, rst_n, result);
+    comp_8 comp_8(ina, inb, clk, rst_n, g, e);
 
     initial begin
-	$dumpfile("add_32sim.vcd");
+	$dumpfile("comp_8sim.vcd");
 	$dumpvars;
-        $monitor("%t: %h + %h => %h(%h)", $time, ina, inb, result, inab);
+        $monitor("%t: %h %h => %b %b", $time, ina, inb, g, e);
 	clk <= 0;
 	rst_n <= 0;
     end
 
     initial begin
         #100 
-	    ina <= 32'h00000001;
-	    inb <= 32'h00000001;
+	    ina <= 8'b00000001;
+	    inb <= 8'b00000001;
 	#20
 	    rst_n <= 1;
 	#100
 	    rst_n <= 0;
 	#100
-	    ina <= 32'h03fd0010;
-	    inb <= 32'h01da0100;
+	    ina <= 8'b00000010;
+	    inb <= 8'b00000100;
 	#20
 	    rst_n <= 1;
 	#200
@@ -35,3 +32,43 @@ module add_32sim();
 
     always #10 clk <= ~clk;
 endmodule
+
+// module add_32sim();
+//     reg [32-1:0] ina, inb;
+//     reg clk, rst_n;
+//     wire [64-1:0] result;
+//     wire [64-1:0] inab;
+//     wire g, e;
+// 
+//     assign inab = ina + inb;
+// 
+//     add_32 add_32(ina, inb, clk, rst_n, result);
+// //    comp_8 comp_8(ina, inb, clk, rst_n, g, e);
+// 
+//     initial begin
+// 	$dumpfile("add_32sim.vcd");
+// 	$dumpvars;
+//         $monitor("%t: %h + %h => %h(%h)", $time, ina, inb, result, inab);
+// 	clk <= 0;
+// 	rst_n <= 0;
+//     end
+// 
+//     initial begin
+//         #100 
+// 	    ina <= 32'h00000001;
+// 	    inb <= 32'h00000001;
+// 	#20
+// 	    rst_n <= 1;
+// 	#100
+// 	    rst_n <= 0;
+// 	#100
+// 	    ina <= 32'h03fd0010;
+// 	    inb <= 32'h01da0100;
+// 	#20
+// 	    rst_n <= 1;
+// 	#200
+// 	    $finish;
+//     end
+// 
+//     always #10 clk <= ~clk;
+// endmodule
